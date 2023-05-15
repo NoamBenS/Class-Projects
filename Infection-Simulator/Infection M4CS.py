4753f817d14d11d449435dd8eb36d6632d72cd63
NoamBenS <noampvb@gmail.com>
2023-05-14 22:15:52 -0400

    Update Infection M4CS.py

--- Infection-Simulator/Infection M4CS.py
+++ Infection-Simulator/Infection M4CS.py
@@ -1,67 +1,69 @@
 from math import sqrt
 import matplotlib.pyplot as plt
-
 import random
 
+
 def simulate_epidemic(G, seed, t, p):
-    
-    return -1
-    #returns one thing: the # of infected
+    infectedGraph = G
+    infected = set()
+    for i in seed:
+        q = G[random.random() * (len(G) - 1)]
+        if infected.__contains__(q):
+            i -= 1
+        infected.add(q)
+
+    rand = float(random.random())
+    float(p)
+    if (rand <= p and i==1):
+        #this
+
+    return 1
+    # returns one thing: the # of infected
 
 
-def generate_random_graph(n,p):
-    size = sqrt(n)
-    size = int(size+.5)
-    numberToBeLinked = n*p
-    zoinks = int(numberToBeLinked)
+def generate_random_graph(n, p):
     ls = []
     for i in range(n):
-        #I want to make an n by n array of nodes of type zero.
-        #As they enter, they should be multiplied by the probability
-        if(zoinks!=0):
-            ls.append(1)
-            zoinks-=1
-        else:
-            ls.append(0)
-    random.shuffle(ls)
-    output = []
-    temp = []
-    count = 0
-    for i in ls:
-        temp.append(i)
-        print(temp)
-        count+=1
-        if(count==size):
-            output.append(temp)
-            count = 0
-            temp.clear()
-
-    return output
+        temp = [0]*n
+        ls.append(temp)
+    for i in range(n):
+        for j in range(n):
+            if ls[i][j] == 1 or i == j:
+                q = 0
+            else:
+                rand = float(random.random())
+                float(p)
+                if (rand <= p):
+                    ls[i][j] = 1
+                    ls[j][i] = 1
+    for i in range(n):
+        print(ls[i])
+    return ls
 
 
 def average_plots():
     p1 = [0, .1, .2, .3, .4, .5, .6, .7, .8, .9, 1]
     p2 = [0, .1, .2, .3, .4, .5, .6, .7, .8, .9, 1]
-    lsx = []
-    lsy = []
+    lsx1 = []
+    lsx2 = []
     lsz = []
-    for i in range(len(p1)): #for every p1
-        n = generate_random_graph(10, p1[0]) #create a new graph
-        for j in range(len(p1)): #then for every in p2
-            infected = 0 # set variables to 0
+    for i in range(len(p1)):  # for every p1
+        n = generate_random_graph(10, p1[i])  # create a new graph
+        for j in range(len(p2)):  # then for every in p2
+            infected = 0  # set variables to 0
             healthy = 0
-            for i in range(99): #run this next part 100 times
-                infected += simulate_epidemic(n, {1}, 1, p2) #run this epidemic thing and add the value to the infected
+            for k in range(100):  # run this next part 100 times
+                infected += simulate_epidemic(n, {1}, 1, p2)  # run this epidemic thing and add the value to the infected
                 healthy += 10 - infected
-            infected_avg = infected/100 #get avg
-            healthy_avg = healthy/100 #get avg
-            lsx.append(infected_avg) #add avg to infected
-            lsy.append(healthy_avg) #add avg to healthy
+            infected_avg = infected / 100  # get avg
+            healthy_avg = healthy / 100  # get avg
+            lsx1.append(infected_avg)  # add avg to infected
+            lsx2.append(healthy_avg)  # add avg to healthy
             lsz.append(p2[j])
-    plt.scatter(lsx, lsz)
-    plt.scatter(lsy, lsz)
+    plt.scatter(lsz, lsx1, color='hotpink') #average infected
+    plt.scatter(lsz, lsx2, color='lightblue')
     plt.show()
     return
 
-#print(generate_random_graph(10,.2))
-average_plots()
+generate_random_graph(10, .2)
+#average_plots()
